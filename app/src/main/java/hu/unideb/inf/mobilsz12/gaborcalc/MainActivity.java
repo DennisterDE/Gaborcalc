3ba1f600cbc25d5609b7ce22f6ca7947c9a5a150
Denister <denister@mailbox.unideb.hu>
2022-09-18 19:18:04 +0200

    első gábor hang rákötése a gomb1 re,

--- app/src/main/java/hu/unideb/inf/mobilsz12/gaborcalc/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/mobilsz12/gaborcalc/MainActivity.java
@@ -2,13 +2,25 @@ package hu.unideb.inf.mobilsz12.gaborcalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button gomb1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gomb1= (Button)findViewById(R.id.button1);
+        final MediaPlayer mp = MediaPlayer.create(this,R.raw.gabor);
+        gomb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            mp.start();
+            }
+        });
+
     }
 }
